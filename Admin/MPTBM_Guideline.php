9f2f3dbb310ab89f7d3ac2090237460bf46e4c74
sumonazma <engr.sumonazma@gmail.com>
2024-01-09 13:56:39 +0600

    guieline issue:01_09

--- Admin/MPTBM_Guideline.php
+++ Admin/MPTBM_Guideline.php
@@ -13,7 +13,7 @@
 			}
 			public function guideline_menu() {
 				$cpt = MPTBM_Function::get_cpt();
-				add_submenu_page('edit.php?post_type=' . $cpt, esc_html__('Guideline', 'ecab-taxi-booking-manager'), '<span>'.esc_html__('Guideline', 'ecab-taxi-booking-manager').'</span>', 'manage_options', 'mptbm_guideline_page', array($this, 'guideline_page'));
+				add_submenu_page('edit.php?post_type=' . $cpt, esc_html__('Guideline', 'ecab-taxi-booking-manager'), '<span>' . esc_html__('Guideline', 'ecab-taxi-booking-manager') . '</span>', 'manage_options', 'mptbm_guideline_page', array($this, 'guideline_page'));
 			}
 			public function guideline_page() {
 				$label = MPTBM_Function::get_name();
@@ -23,11 +23,31 @@
 					<div class=_dShadow_6_adminLayout">
 						<h2 class="textCenter"><?php echo esc_html($label) . '  ' . esc_html__('available Shortcode', 'ecab-taxi-booking-manager'); ?></h2>
 						<div class="divider"></div>
-						<ul>
-							<li>
-								<pre>[mptbm_booking]</pre>
-							</li>
-						</ul>
+						<table class="table table-striped table-bordered" style="background:#EEF5E4;border-radius:10px;">
+							<tbody>
+							<tr>
+								<td>Shortcode:</td>
+								<td colspan="2"><code>[mptbm_booking price_based='dynamic' form='horizontal' progressbar='yes' map='yes']</code></td>
+							</tr>
+							<tr>
+								<td rowspan="4">Parameters:</td>
+								<td><code> price_based</code></td>
+								<td>this should be <strong>manual/dynamic</strong> default is <strong>dynamic</strong> pricing will work based on google map distance but manual means fixed pricing between 2 location</td>
+							</tr>
+							<tr>
+								<td><code>form</code></td>
+								<td><strong>inline</strong> or <strong>horizontal</strong> default <strong>horizontal</strong> and inline means minimal single line form</td>
+							</tr>
+							<tr>
+								<td><code>progressbar</code></td>
+								<td><strong>yes</strong> or <strong>no</strong> default <strong>yes</strong> . if no then progressbar will be hidden</td>
+							</tr>
+							<tr>
+								<td><code>map</code></td>
+								<td><strong>yes</strong> or <strong>no</strong> default <strong>yes</strong> .if no then map will be hidden</td>
+							</tr>
+							</tbody>
+						</table>
 					</div>
 				</div>
 				<?php
